f07e13d750a0997ef62f15826f7790c0fbadb814
Kennedy Ouma Odero <kennedyrogouma222@gmail.com>
2025-07-24 16:04:11 +0300

    merge conflicts

--- src/App.jsx
+++ src/App.jsx
@@ -1,87 +1,41 @@
+
+
+// import Footer from "@/components/common/Footer";
+// import LearnerLeaderboardPage from "@/pages/learner/Leaderboard.jsx";
+// import Navbar from "@/components/common/Navbar";
+
+
+
+
 import "./App.css";
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
 
-import React, { useEffect } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
-//import Home from "./pages/learner/Home";
-
-
-
-import Path from './pages/learner/Path';
-
-
-
-
-// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
-
-// // Routes
-// import LearnerRoutes from "./routes/LearnerRoutes.jsx";
-// import AdminRoutes from "./routes/AdminRoutes.jsx";
-// import ContributorRoutes from "./routes/ContributorRoutes.jsx";
-// // import ProtectedRoute from "./routes/ProtectedRoute.jsx";
-
-// // Common Layout Components
-// import Navbar from "./components/common/Navbar.jsx";
-// import Footer from "./components/common/Footer.jsx";
-
-// // Routes
-// import ClientRoutes from "./routes/ClientRoutes.jsx";
-// import AdminRoutes from "./routes/AdminRoutes.jsx";
-
-//import Badges from './pages/learner/Badges';
-//import AdminSidebar from './components/admin/AdminPanel';
-
-
-//import ContributorSidebar from './components/contributor/contributor-sidebar';
-
-//import AdminSidebar from './components/admin/AdminPanel';
-
-
-//import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
-
-import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
-// import { AppSidebar } from "@/components/common/AppSidebar.jsx";
-import AppSidebar from "@/components/common/AppSidebar";
-import Navbar from "@/components/common/Navbar";
-import Footer from "@/components/common/Footer";
-import LearnerLeaderboardPage from "../pages/learner/Leaderboard.jsx";
-import LearnerHomePage from "./pages/learner/Home.jsx";
-import LearnerPathPage from "./pages/learner/Path.jsx";
-import LearnerBadgesPage from "./pages/learner/Badges.jsx";
-import LearnerCoursesPage from "./pages/learner/Courses.jsx";
-import LearnerQuizzesPage from "./pages/learner/Quizzes.jsx";
-import LearnerProfilePage from "./pages/learner/Profile.jsx";
-import LearnerResourcesPage from "./pages/learner/Resources.jsx";
-import LearnerTestimonialsPage from "./pages/learner/Testimonials.jsx";
-import LearnerTeamPage from "./pages/learner/Team.jsx";
-import LearnerPricingPage from "./pages/learner/Pricing.jsx";
-import LearnerContactPage from "./pages/learner/Contact.jsx";
-import LearnerHome from "./pages/learner/Home.jsx";
-
-
-
-// 👇 Import your landing page
-import Home from "./pages/learner/Home.jsx";
 // Routes
 import LearnerRoutes from "./routes/LearnerRoutes.jsx";
+import { SidebarProvider } from "@/components/ui/sidebar";
 
 
 const App = () => {
   return (
-    <div className="flex flex-col min-h-screen">
-      {/* Navbar */}
-      <Navbar />
+    // <div className="flex flex-col min-h-screen">
+    //   {/* Navbar */}
+    //   <Navbar />
 
-      {/* Footer */}
-      <Footer />
+    //   {/* Footer */}
+    //   <Footer />
 
-      {/* leaderboard page */}
-      <LearnerLeaderboardPage className="flex-grow bg-[#F9FAFB]" />
-    </div>
+    //   {/* leaderboard page */}
+    //   <LearnerLeaderboardPage className="flex-grow bg-[#F9FAFB]" />
+    // </div>
+    <SidebarProvider>
+      <Router>
+        <LearnerRoutes />
+      </Router>
+    </SidebarProvider>
   );
 };
+
 export default App;
 
 
--- src/routes/LearnerRoutes.jsx
+++ src/routes/LearnerRoutes.jsx
@@ -10,9 +10,11 @@ import LearnerCommunityPage from "../pages/learner/Community";
 import LearnerDashboardPage from "../pages/learner/Dashboard";
 import LearnerHomePage from "../pages/learner/Home";
 import LearnerLeaderboardPage from "../pages/learner/Leaderboard";
+import LearnerPaymentPage from "../pages/learner/Payment";
+
 import LearnerAuthPage from "../pages/learner/Auth";
 import LearnerPathPage from "../pages/learner/Path";
-// import LearnerProfilePage from "../pages/learner/Profile";
+import LearnerProfilePage from "../pages/learner/Profile";
 import LearnerQuizesPage from "../pages/learner/Quizes";
 import LearnerSettingsPage from "../pages/learner/Settings";
 
@@ -21,7 +23,7 @@ const LearnerRoutes = () => {
     <Routes>
       {/* Public layout for home + auth */}
       <Route element={<PublicLayout />}>
-        <Route path="/home" element={<LearnerHomePage />} />
+        <Route path="/*" element={<LearnerHomePage />} />
         <Route path="/auth" element={<LearnerAuthPage />} />
       </Route>
 
@@ -30,9 +32,10 @@ const LearnerRoutes = () => {
         <Route path="/dashboard" element={<LearnerDashboardPage />} />
         <Route path="/badges" element={<LearnerBadgesPage />} />
         <Route path="/community" element={<LearnerCommunityPage />} />
+        <Route path="/payment" element={<LearnerPaymentPage />} />
         <Route path="/leaderboard" element={<LearnerLeaderboardPage />} />
         <Route path="/path" element={<LearnerPathPage />} />
-        {/* <Route path="/profile" element={<LearnerProfilePage />} /> */}
+         <Route path="/profile" element={<LearnerProfilePage />} />
         <Route path="/quizes" element={<LearnerQuizesPage />} />
         <Route path="/settings" element={<LearnerSettingsPage />} />
       </Route>
